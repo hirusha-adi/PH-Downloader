564f4868a19d2f7e532d17563247504b4c515a8f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2021-10-13 18:09:52 +0530

    supports low quality downloads

---
+++ PHbackend.py
@@ -0,0 +1,198 @@
+import os
+import platform
+try:
+    import requests
+except:
+    if platform.system().lower().startswith('win'):
+        os.system("pip install youtube-dl")
+    else:
+        os.system("pip3 install youtube-dl")
+    import requests
+
+try:
+    import delegator
+except:
+    if platform.system().lower().startswith('win'):
+        os.system("pip install delegator")
+    else:
+        os.system("pip3 install delegator")
+    import delegator
+
+try:
+    import crayons
+except:
+    if platform.system().lower().startswith('win'):
+        os.system("pip install crayons")
+    else:
+        os.system("pip3 install crayons")
+    import crayons
+
+try:
+    from bs4 import BeautifulSoup
+except:
+    if platform.system().lower().startswith('win'):
+        os.system("pip install bs4")
+    else:
+        os.system("pip3 install bs4")
+    from bs4 import BeautifulSoup
+
+
+session = requests.Session()
+
+delegator.TIMEOUT = 600
+
+class Video:
+
+    def __init__(self, ph, title, url, category=None):
+        self.ph = ph
+        self.title = title
+        self.url = url
+        self.category = category
+        self._meta = {}
+
+    def __repr__(self):
+        return "<Video title='{}'>".format(self.title)
+
+    @property
+    def meta(self):
+        self._get_metadata()
+        return self._meta
+
+    def _get_metadata(self):
+        soup = self.ph._get_bs(self.url)
+
+        count = soup.find('span', class_='count').text
+        self._meta['count'] = int(count.replace(',', ''))
+
+        percent = soup.find('span', class_='percent').text[:-1]
+        self._meta['percent'] = float('0.{}'.format(percent))
+
+        try:
+            cats = soup.find('div', class_='categoriesWrapper').text.split()[1:-2]
+            self._meta['category_names'] = cats
+
+        except AttributeError:
+            self._meta['category_names'] = []
+
+    def download(self, block=True):
+        print(crayons.red('Downloading video…'))
+        c = delegator.run('youtube-dl -f 22 {}'.format(self.url), block=block)
+        return c
+
+    def download_720p(self, quality: str = None, block=True):
+        print(crayons.red('Downloading video…'))
+        c = delegator.run('youtube-dl -f 22 {}'.format(self.url), block=block)
+        return c
+    
+    def download_480p(self, quality: str = None, block=True):
+        print(crayons.red('Downloading video…'))
+        c = delegator.run('youtube-dl -f 135 {}'.format(self.url), block=block)
+        return c
+    
+    def download_360p(self, quality: str = None, block=True):
+        print(crayons.red('Downloading video…'))
+        c = delegator.run('youtube-dl -f 134 {}'.format(self.url), block=block)
+        return c
+
+
+class Category:
+    def __init__(self, ph, title, url):
+        self.ph = ph
+        self.title = title
+        self.url = url
+
+    def __repr__(self):
+        return "<Category title='{}'>".format(self.title)
+
+    @property
+    def id(self):
+        try:
+            return self.url.split('=')[1]
+        except IndexError:
+            return self.url.split('/')[-1]
+
+    def __iter__(self):
+        return (v for v in self.videos())
+
+    def videos(self, max=None):
+
+        def gen(url):
+            soup = self.ph._get_bs(url)
+
+            next = soup.find('li', class_='page_next').find('a')['href']
+
+            for span in soup.find_all('span', class_='title'):
+
+                if span.find('a'):
+                    title = span.find('a')['title']
+                    url = self.ph.base_url + span.find('a')['href']
+
+                    yield Video(ph=self.ph, title=title, url=url, category=self)
+
+            yield from gen(next)
+
+        i = 0
+        for v in gen(self.url):
+            i += 1
+            if max is None or i <= max:
+                yield v
+            else:
+                break
+
+
+class PornHub:
+    def __init__(self):
+        self.base_url = 'https://www.pornhub.com'
+
+    def _get(self, url, *args, **kwargs):
+        if not url.startswith('http'):
+            url = '{}{}'.format(self.base_url, url)
+
+        return session.get(url, *args, **kwargs)
+
+    def _get_bs(self, url, *args, **kwargs):
+        r = self._get(url, *args, **kwargs)
+        return BeautifulSoup(r.content, 'lxml')
+
+    def __getitem__(self, key):
+        for cat in self.categories:
+            if cat.title.lower() == key.lower():
+                return cat
+
+    def search(self, s, max=None):
+        def gen(url):
+            soup = self._get_bs(url, params={'search': s})
+            next = soup.find('li', class_='page_next').find('a')['href']
+
+            for span in soup.find_all('span', class_='title'):
+
+                if span.find('a'):
+                    title = span.find('a')['title']
+                    url = self.base_url + span.find('a')['href']
+
+                    yield Video(ph=self, title=title, url=url)
+
+            yield from gen(next)
+
+        i = 0
+        for v in gen('/video/search'):
+            i += 1
+            if max is None or i <= max:
+                yield v
+            else:
+                break
+
+    @property
+    def categories(self):
+        soup = self._get_bs('/categories')
+        categories = []
+        for div in soup.find_all('div', class_='category-wrapper'):
+            title = div.find('a')['alt']
+            url = self.base_url + div.find('a')['href']
+
+            c = Category(ph=self, title=title, url=url)
+
+            categories.append(c)
+
+        return categories
+
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 import platform
-from phlib import PornHub
-
+from PHbackend import PornHub
+try:
+    import youtube_dl
+except:
+    if platform.system().lower().startswith('win'):
+        os.system("pip install youtube-dl")
+    else:
+        os.system("pip3 install youtube-dl")
 
 
 def clear_screen():
@@ -15,11 +21,7 @@ def show_logo(clrs: bool = True):
     if clrs:
         clear_screen()
     print("""
- _            _   
-| |_ ___  ___| |_ 
-| __/ _ \/ __| __|
-| ||  __/\__ \ |_ 
- \__\___||___/\__|
+PORN HUB DOWNLOADER v0.1
     """)
 
 
@@ -36,8 +38,18 @@ def ENTIRE_PROGRAM():
     print("""
     [1] List categories 
     [2] Search by word
+
     [3] Mass Download
     [4] Search and Download
+
+    [5] Mass Download (720p)
+    [6] Search and Download (720p)
+
+    [7] Mass Download (480p)
+    [8] Search and Download (480p)
+
+    [9] Mass Download (360p)
+    [10] Search and Download (360p)
     """)
 
     main_menu_opt = input("? Select an option: ")
@@ -90,6 +102,102 @@ def ENTIRE_PROGRAM():
             print("! Downloading video...")
             ph_result.download()
             print("+ Download complete!")
+
+    # Mass Downloader (720p)
+    if main_menu_opt == "5":
+        search_for = input("? What to search: ")
+        search_amout = int(input("? How many: "))
+        ph_results = ph.search(search_for, max=search_amout)
+        for ph_result in ph_results:
+            print("")
+            print(f"---------------\n{ph_result.title}\n+ URL: {ph_result.url}\n+ Category: {ph_result.category}")
+            print(f"+ View Count: {ph_result.meta['count']}\n+ Like Percentage: {ph_result.meta['percent']}%\n+ Categories:")
+            for category in ph_result.meta['category_names']:
+                print(category, end=", ")
+            print("")
+            print("! Downloading video...")
+            ph_result.download_720p()
+            print("+ Download complete!")
+
+    # Search and Download (720p)
+    if main_menu_opt == "6":
+        search_for = input("? What to search: ")
+        ph_results = ph.search(search_for, max=1)
+        for ph_result in ph_results:
+            print("")
+            print(f"---------------\n{ph_result.title}\n+ URL: {ph_result.url}\n+ Category: {ph_result.category}")
+            print(f"+ View Count: {ph_result.meta['count']}\n+ Like Percentage: {ph_result.meta['percent']}%\n+ Categories:")
+            for category in ph_result.meta['category_names']:
+                print(category, end=", ")
+            print("")
+            print("! Downloading video...")
+            ph_result.download_720p()
+            print("+ Download complete!")
+    
+    # Mass Downloader (480p)
+    if main_menu_opt == "7":
+        search_for = input("? What to search: ")
+        search_amout = int(input("? How many: "))
+        ph_results = ph.search(search_for, max=search_amout)
+        for ph_result in ph_results:
+            print("")
+            print(f"---------------\n{ph_result.title}\n+ URL: {ph_result.url}\n+ Category: {ph_result.category}")
+            print(f"+ View Count: {ph_result.meta['count']}\n+ Like Percentage: {ph_result.meta['percent']}%\n+ Categories:")
+            for category in ph_result.meta['category_names']:
+                print(category, end=", ")
+            print("")
+            print("! Downloading video...")
+            ph_result.download_480p()
+            print("+ Download complete!")
+
+    # Search and Download (480p)
+    if main_menu_opt == "8":
+        search_for = input("? What to search: ")
+        ph_results = ph.search(search_for, max=1)
+        for ph_result in ph_results:
+            print("")
+            print(f"---------------\n{ph_result.title}\n+ URL: {ph_result.url}\n+ Category: {ph_result.category}")
+            print(f"+ View Count: {ph_result.meta['count']}\n+ Like Percentage: {ph_result.meta['percent']}%\n+ Categories:")
+            for category in ph_result.meta['category_names']:
+                print(category, end=", ")
+            print("")
+            print("! Downloading video...")
+            ph_result.download_480p()
+            print("+ Download complete!")
+    
+    # Mass Downloader (360p)
+    if main_menu_opt == "9":
+        search_for = input("? What to search: ")
+        search_amout = int(input("? How many: "))
+        ph_results = ph.search(search_for, max=search_amout)
+        for ph_result in ph_results:
+            print("")
+            print(f"---------------\n{ph_result.title}\n+ URL: {ph_result.url}\n+ Category: {ph_result.category}")
+            print(f"+ View Count: {ph_result.meta['count']}\n+ Like Percentage: {ph_result.meta['percent']}%\n+ Categories:")
+            for category in ph_result.meta['category_names']:
+                print(category, end=", ")
+            print("")
+            print("! Downloading video...")
+            ph_result.download_360p()
+            print("+ Download complete!")
+
+    # Search and Download (360p)
+    if main_menu_opt == "10":
+        search_for = input("? What to search: ")
+        ph_results = ph.search(search_for, max=1)
+        for ph_result in ph_results:
+            print("")
+            print(f"---------------\n{ph_result.title}\n+ URL: {ph_result.url}\n+ Category: {ph_result.category}")
+            print(f"+ View Count: {ph_result.meta['count']}\n+ Like Percentage: {ph_result.meta['percent']}%\n+ Categories:")
+            for category in ph_result.meta['category_names']:
+                print(category, end=", ")
+            print("")
+            print("! Downloading video...")
+            ph_result.download_360p()
+            print("+ Download complete!")
+
+
+
 
 
 
